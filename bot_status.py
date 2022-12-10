dbe0184e90e24768d8564f9fcc0f3983c9c49127
Fescalzinh8 <120221138+Fescalzinh8@users.noreply.github.com>
2022-12-10 16:04:27 -0300

    Add files via upload

--- bot_status.py
+++ bot_status.py
@@ -1,59 +1,53 @@
-"""
-Este plugin executa antes dos outros e em caso do bot estar em manutenção,
-exibe uma mensagem de aviso e cancelando o processamento da mensagem pelo bot.
-"""
-
-
 from pyrogram import Client, filters
-from pyrogram.types import (
-    CallbackQuery,
-    InlineQuery,
-    InlineQueryResultArticle,
-    InputTextMessageContent,
-    Message,
-)
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import ADMINS
-from utils import get_news_user, is_bot_online
+from database import cur, db
+from utils import is_bot_online
 
 
-@Client.on_message(
-    ~filters.user(ADMINS) & filters.regex(r"^/") & filters.private, group=-1
-)
-async def bot_status_msg(c: Client, m: Message):
-    if not is_bot_online():
-        news_user = get_news_user()
-        await m.reply_text(
-            f"<b>⚠ Bot em manutenção, tente novamente mais tarde.</b> Para saber as novidades do bot, entre em {news_user}."
-        )
-        await m.stop_propagation()
+def set_bot_status(status: bool):
+    """Define o status do bot para o status definido."""
+
+    cur.execute("UPDATE bot_config SET is_on = ? WHERE ROWID = 0", (status,))
+    db.commit()
 
 
-@Client.on_callback_query(~filters.user(ADMINS), group=-1)
-async def bot_status_cq(c: Client, m: CallbackQuery):
-    if not is_bot_online():
-        news_user = get_news_user()
-        await m.answer(
-            f"<b>⚠ Bot em manutenção, tente novamente mais tarde.</b> Para saber as novidades do bot, entre em {news_user}.",
-            show_alert=True,
-            cache_time=5,
-        )
-        await m.stop_propagation()
+@Client.on_callback_query(filters.regex("^bot_status$") & filters.user(ADMINS))
+async def bot_status(c: Client, m: CallbackQuery):
+    is_on = is_bot_online()
 
+    status = "✅ on" if is_on else "❌ off"
+    reverse = "❌ off" if is_on else "✅ on"
 
-@Client.on_inline_query(~filters.user(ADMINS), group=-1)
-async def bot_status_inline(c: Client, m: InlineQuery):
-    if not is_bot_online():
-        news_user = get_news_user()
-        results = [
-            InlineQueryResultArticle(
-                title="⚠ Bot em manutenção.",
-                description="Tente novamente mais tarde.",
-                input_message_content=InputTextMessageContent(
-                    f"<b>⚠ Bot em manutenção, tente novamente mais tarde.</b> Para saber as novidades do bot, entre em {news_user}."
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    reverse, callback_data="change_status " + reverse.split()[-1]
                 ),
-            )
+            ],
+            [InlineKeyboardButton("🔙 Voltar", callback_data="painel")],
         ]
+    )
 
-        await m.answer(results, cache_time=5)
-        await m.stop_propagation()
+    await m.edit_message_text(
+        f"<b>🚦 Status do bot: </b>\n"
+        "<i>- Esta opção controla se o bot responderá a requisições feitas por usuários comuns (não admins) ou não.</i>\n\n"
+        f"<b>Status atual:</b> {status}",
+        reply_markup=kb,
+    )
+
+
+@Client.on_callback_query(
+    filters.regex(r"^change_status (?P<status>.+)") & filters.user(ADMINS)
+)
+async def change_status(c: Client, m: CallbackQuery):
+    status = m.matches[0]["status"] == "on"
+
+    set_bot_status(status)
+
+    await m.answer(f"✅ O status foi definido para {status}")
+
+    # Directly call the function again to edit callback data after the change.
+    await bot_status(c, m)
